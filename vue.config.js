ae739104701f0267cdd31d71059a816809c8b4e9
Grigory <mikigrigorij@yandex.ru>
2025-11-03 09:39:44 +0300

    Настройка github pages

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
+  filenameHashing: false,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/training-app-2/dist/'
+    ? '/training-app-2/docs/'
     : '/'
 });
